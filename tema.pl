417a472e5b83b7b25b3e94ee5325fba5843c111b
TheTugaOne <raduradu0410@yahoo.com>
2017-05-16 13:24:33 +0300

    Update tema.pl

--- tema.pl
+++ tema.pl
@@ -1,3 +1,17 @@
+2.
 sublist(X,Y):-append(X,_,Y).
 sublist(X,Y):-append(_,X,Y).
 sublist(X,Y):-append(X,_,Z),append(_,Z,Y).
+
+8.
+stanga(st).
+dreapta(dr).
+rotirest([E|T], R) :-append(T,[E],R).
+rotiredr(L, R) :-rotirest(R, L).
+rotire(Lista,_,0,Lista).
+rotire(Lista,Directie,Nr,ListaRez):-
+    stanga(Directie),
+	rotirest(Lista,L),Nr1 is Nr-1,rotire(L,st,Nr1,ListaRez).
+rotire(Lista,Directie,Nr,ListaRez):-
+    dreapta(Directie),
+	rotiredr(Lista,L),Nr1 is Nr-1,rotire(L,dr,Nr1,ListaRez).
